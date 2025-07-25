8a778533e50d8afb8ff5af84dd766d4029708f35
ambrojobs <89398005+NikoZ21@users.noreply.github.com>
2025-07-25 15:14:59 +0400

    added configs

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace freeschool_discipline_back.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
