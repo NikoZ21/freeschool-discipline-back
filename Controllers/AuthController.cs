fd4d698a05d4da6369155b1a24d0cd686d0728b0
ambrojobs <89398005+NikoZ21@users.noreply.github.com>
2025-08-25 02:08:01 +0400

    added scalar for API testing

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using DisciplineBackend_WebApi.Entities;
+using DisciplineBackend_WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using System.Net.NetworkInformation;
+
+
+namespace DisciplineBackend_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        public User user = new();
+
+        [HttpPost("register")]
+        public IActionResult Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PsswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+    }
+}
