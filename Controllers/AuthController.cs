0a505ce8e6bb31ffdc531c76839b37d82e61ceae
ambrojobs <89398005+NikoZ21@users.noreply.github.com>
2025-08-26 15:38:35 +0400

    added token successfully

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -4,14 +4,18 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net.NetworkInformation;
+using System.Security.Claims;
+using System.Text;
 
 
 namespace DisciplineBackend_WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController : ControllerBase
+    public class AuthController(IConfiguration configuration) : ControllerBase
     {
 
         private readonly User user = new() { Username = "peter", PsswordHash = "AQAAAAIAAYagAAAAEJMFMr3DdNRNE/BQT30a5nWXyRXz5024sonbWD7tYMi+da+9Xw6HRybe49UoVEidnA==" };
@@ -41,9 +45,26 @@ namespace DisciplineBackend_WebApi.Controllers
                 return BadRequest("Wrong password.");
             }
 
-            string token = "success"; // Placeholder for token generation logic
+            string token = CreateToken(user);
 
             return Ok(token);
         }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(issuer: configuration.GetValue<string>("AppSettings:Issuer"), audience: configuration.GetValue<string>("AppSettings:Audiance"), claims: claims, expires: DateTime.UtcNow.AddDays(1), signingCredentials: creds);
+
+            return new  JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+
     }
 }
